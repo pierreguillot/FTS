85353231c44ce94aa82e47ca709c8d646518860d
IOhannes m zmölnig <zmoelnig@iem.at>
2019-02-21 13:28:42 +0100

    refactored 2nd opcodes-test into separate function

--- tools/FstHost/FstHost.cpp
+++ tools/FstHost/FstHost.cpp
@@ -315,20 +315,8 @@ void test_opcode29(AEffect*effect) {
   }
 }
 
-int test_plugin(const char*filename) {
-  t_fstMain*vstmain = load_plugin(filename);
-  if(!vstmain)return printf("'%s' was not loaded\n", filename);
-  AEffect*effect = vstmain(&dispatcher);
-  printf("instantiated effect %p\n", effect);
-  if(!effect)return printf("unable to instantiate plugin from '%s'\n", filename);
-  //dump_data(filename, effect, 160);
-  if(effect->magic != 0x56737450) return printf("magic failed: 0x%08X", effect->magic);
-  print_aeffect(effect);
-  //test_reaper(effect); return 0;
-  //test_opcode29(effect); return 0;
-
-
-  for(size_t opcode=16; opcode<63; opcode++) {
+void test_opcodesJUCE(AEffect*effect) {
+ for(size_t opcode=16; opcode<63; opcode++) {
     if(skipOpcodeJUCE(opcode))continue;
     char buffer[200] = { 0 };
     t_fstPtrInt result = dispatch(effect, opcode, 0, 0, buffer, 0.f);
@@ -343,6 +331,21 @@ int test_plugin(const char*filename) {
     if(result || *buffer)
       printf("\n");
   }
+}
+
+int test_plugin(const char*filename) {
+  t_fstMain*vstmain = load_plugin(filename);
+  if(!vstmain)return printf("'%s' was not loaded\n", filename);
+  AEffect*effect = vstmain(&dispatcher);
+  printf("instantiated effect %p\n", effect);
+  if(!effect)return printf("unable to instantiate plugin from '%s'\n", filename);
+  //dump_data(filename, effect, 160);
+  if(effect->magic != 0x56737450) return printf("magic failed: 0x%08X", effect->magic);
+  print_aeffect(effect);
+  //test_reaper(effect); return 0;
+  //test_opcode29(effect); return 0;
+
+  test_opcodesJUCE(effect);
 
   //test_opcodes(effect, 78);
   return 0;
