7ec006e81bd3741d19a09bb50c56f86c6da3176c
IOhannes m zmölnig <zmoelnig@iem.at>
2019-02-21 13:27:15 +0100

    don't run reaper-test

--- tools/FstHost/FstHost.cpp
+++ tools/FstHost/FstHost.cpp
@@ -324,9 +324,9 @@ int test_plugin(const char*filename) {
   //dump_data(filename, effect, 160);
   if(effect->magic != 0x56737450) return printf("magic failed: 0x%08X", effect->magic);
   print_aeffect(effect);
-  test_reaper(effect);
-  return 0;
+  //test_reaper(effect); return 0;
   //test_opcode29(effect); return 0;
+
 
   for(size_t opcode=16; opcode<63; opcode++) {
     if(skipOpcodeJUCE(opcode))continue;
