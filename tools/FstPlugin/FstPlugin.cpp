2fe6de9a3b6bbca5673e251f31bf7359b902e002
IOhannes m zmölnig <zmoelnig@iem.at>
2019-02-25 11:51:04 +0100

    use names for effFlags

--- tools/FstPlugin/FstPlugin.cpp
+++ tools/FstPlugin/FstPlugin.cpp
@@ -253,9 +253,9 @@ AEffect*VSTPluginMain(AEffectDispatcherProc dispatch4host) {
   eff->version = 666;
 
   eff->flags |= effFlagsProgramChunks;
-  eff->flags |= (1<< 4); // replacing
+  eff->flags |= effFlagsCanReplacing;
   //eff->flags |= (1<< 9);
-  eff->flags |= (1<<12); // double replacing
+  eff->flags |= effFlagsCanDoubleReplacing;
 
   eff->processReplacing = processReplacing;
   eff->processDoubleReplacing = processDoubleReplacing;
