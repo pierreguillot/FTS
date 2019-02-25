5fdcfe70ee935b50181e65fc9cf942aa91223c6c
IOhannes m zmölnig <zmoelnig@iem.at>
2019-02-25 16:12:42 +0100

    fixed hex2bin script

--- tools/hexdump2bin.py
+++ tools/hexdump2bin.py
@@ -2,7 +2,8 @@
 
 import sys
 
-with open(sys.argv[1]) as f:
+for fname in sys.argv[1:]:
+  with open(fname) as f:
     for line in f.readlines():
-        for x in line.split():
-            sys.stdout.write(chr(int(x, 16)))
+        data = bytes([int(_,16) for _ in line.split()])
+        sys.stdout.buffer.write(data)
