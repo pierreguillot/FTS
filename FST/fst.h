d884879eb751cfcbe9fb4eeb82756ab82125705e
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-03-14 17:41:01 +0100

    GPL3 boilerplate

--- FST/fst.h
+++ FST/fst.h
@@ -1,3 +1,25 @@
+/*
+ * Free Studio Technologies - plugin SDK
+ * a plugin SDK that is compatible with a well-known
+ * but no longer available commercial plugin SDK.
+ *
+ * Copyright © 2019, IOhannes m zmölnig, IEM
+ *
+ * This file is part of FST
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with striem.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #ifndef FST_fst_h_
 #define FST_fst_h_
 
